e98e6bb21f616cf3dcb620d33c5fac687f9fd248
Kirill <1305kirillfomin@gmail.com>
2023-05-05 23:09:57 +0300

    Delete outdated methods

--- WeatherAPI.cpp
+++ WeatherAPI.cpp
@@ -1,9 +1,15 @@
 #include "weatherAPI.h"
-#include <vector>
+#include "curl/curl.h"
+#include <fstream>
+#include <cstddef>
+#include <iostream>
+#include <stdlib.h>
+
+
 Weather::Weather(std::string unprocessedCityData){
-        std::string unprocessedData = request(formatInput(unprocessedCityData));
-        weatherData = nlohmann::json::parse(unprocessedData);
+        weatherData = nlohmann::json::parse(request(formatInput(unprocessedCityData)));
     }
+
 std::string Weather::getAddress(){
         std::string fullAddress(weatherData.value("resolvedAddress", "Error getting precise address"));
         std::string shortAddress = "";
@@ -19,8 +25,8 @@ std::string Weather::getAddress(){
         return shortAddress;
     }
 
-std::string Weather::getValue(int offset, std::string key){
-    std::string value(weatherData["days"][offset][key].dump());
+std::string Weather::getValue(int day, std::string key){
+    std::string value(weatherData["days"][day][key].dump());
     if (value.find('"') != std::string::npos){
         value.erase(0, 1);
         value.erase(value.length() - 1, 2);
@@ -37,8 +43,8 @@ std::string Weather::getCurrentConditions(std::string key){
     return value;
 }
 
-std::string Weather::getHourlyConditions(int offset, int hour, std::string key){
-    std::string value(weatherData["days"][offset]["hours"][hour][key].dump());
+std::string Weather::getHourlyConditions(int day, int hour, std::string key){
+    std::string value(weatherData["days"][day]["hours"][hour][key].dump());
     if (value.find('"') != std::string::npos){
         value.erase(0, 1);
         value.erase(value.length() - 1, 2);
@@ -46,18 +52,14 @@ std::string Weather::getHourlyConditions(int offset, int hour, std::string key){
     return value;
 }
 
-
-
 typedef size_t(*curl_write)(char*, size_t, size_t, std::string*);
 
-std::string request(std::string city) {
+std::string Weather::request(std::string city) {
     CURLcode res_code = CURLE_FAILED_INIT;
     CURL* curl = curl_easy_init();
     std::string result;
     std::string url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?unitGroup=metric&key=LBME92TM9HRUY3VG2TP2D7LKF&iconSet=icons2&contentType=json";
-
     curl_global_init(CURL_GLOBAL_ALL);
-
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER , 0);
@@ -87,7 +89,7 @@ std::string request(std::string city) {
     return result;
 }
 
-std::string& formatInput(std::string& inputString) {
+std::string& Weather::formatInput(std::string& inputString) {
     std::transform(inputString.begin(), inputString.end(), inputString.begin(), ::tolower);
     size_t spacePosition = 0;
     while ((spacePosition = inputString.find(" ")) != std::string::npos) {
@@ -95,41 +97,5 @@ std::string& formatInput(std::string& inputString) {
         inputString.insert(spacePosition, "%20");
     }
     return inputString;
-}
-
-void showForecast(nlohmann::json& OutputData) {
-
-    nlohmann::json OutputDataArray = OutputData["days"];
-    std::cout << "Weather in " << OutputData.value("resolvedAddress", "Error getting precise address") << ":" << std::endl << std::endl;
-    for (size_t i = 0; i < OutputDataArray.size(); i++)
-    {
-        std::cout << OutputDataArray[i].value("datetime", "Error getting date information") << ": " << OutputDataArray[i]["tempmax"] << "   " << OutputDataArray[i]["tempmin"] << std::endl;
-        std::cout << OutputDataArray[i].value("conditions", "Error getting max temperature information") << std::endl << std::endl;
-    }
-}
-
-
-nlohmann::json getWeatherByDay(nlohmann::json& OutputData, int offset) { // offset 0 - today, 1 - tomorrow...
-    nlohmann::json weatherByDay = OutputData["days"][offset];
-    return weatherByDay;
-}
-
-std::string getValue(nlohmann::json& weatherByDay, std::string key) {
-    return std::string(weatherByDay.value(key, "Error getting a value"));
-}
-
-std::string getAddress(nlohmann::json& OutputData){
-    std::string fullAddress(OutputData.value("resolvedAddress", "Error getting precise address"));
-    std::string shortAddress = "";
-    for (size_t index = 0; fullAddress[index]!= ','; ++index){
-        shortAddress += fullAddress[index];
-    }
-    int lastCommaIndex = fullAddress.length() - 1; // last character
-    while (lastCommaIndex != 0 && (fullAddress[lastCommaIndex] != ','))
-    {
-        --lastCommaIndex;
-    }
-    shortAddress += fullAddress.substr(lastCommaIndex); // +1 to skip leading space
-    return shortAddress;
 }
 
--- weatherAPI.h
+++ weatherAPI.h
@@ -1,38 +1,20 @@
-#ifndef WEATHERAPIH
-#define WEATHERAPIH
-#include <cstddef>
-#include <iostream>
-#include <stdlib.h>
-#include <crtdbg.h>
-#include "curl/curl.h"
+#ifndef WEATHERAPI_H
+#define WEATHERAPI_H
 #include <string>
-#include <fstream>
-#include <vector>
 #include <nlohmann/json.hpp>
 
 class Weather {
 public:
     Weather(std::string unprocessedCityData);
     std::string getAddress();
-    std::string getValue(int offset, std::string key);
+    std::string getValue(int day, std::string key);
     std::string getCurrentConditions(std::string key);
-    std::string getHourlyConditions(int offset, int hour, std::string key);
+    std::string getHourlyConditions(int day, int hour, std::string key);
 private:
     nlohmann::json weatherData;
+    std::string request(std::string city);
+    std::string& formatInput(std::string& inputString);
+    std::string dayOfWeek(int year, int month, int day);
 };
-
-std::string request(std::string city);
-
-std::string& formatInput(std::string& inputString);
-
-void showForecast(nlohmann::json& OutputData);
-
-nlohmann::json getWeatherByDay(nlohmann::json& OutputData, int offset);
-
-std::string getValue(nlohmann::json& weatherByDay, std::string key);
-
-std::string getAddress(nlohmann::json& OutputData);
-
-std::string dayOfWeek(int year, int month, int day);
 
 #endif
